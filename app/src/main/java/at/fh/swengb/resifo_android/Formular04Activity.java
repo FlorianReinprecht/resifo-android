b83809b77a1aec96a3af96280984c8834a6d6e80
haxhaagn15 <agnesa.haxha@edu.fh-joanneum.at>
2017-01-05 18:52:48 +0100

    Create first activities

---
+++ app/src/main/java/at/fh/swengb/resifo_android/Formular04Activity.java
@@ -0,0 +1,13 @@
+package at.fh.swengb.resifo_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Formular04Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formular04);
+    }
+}
