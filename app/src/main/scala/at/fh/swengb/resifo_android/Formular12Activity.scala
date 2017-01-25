6be76a3d848a1997e6e5415edf49e5d86668e97f
haxhaagn15 <agnesa.haxha@edu.fh-joanneum.at>
2017-01-25 14:13:16 +0100

    Improve activity layout and add a rating bar V5

--- app/src/main/scala/at/fh/swengb/resifo_android/Formular12Activity.scala
+++ app/src/main/scala/at/fh/swengb/resifo_android/Formular12Activity.scala
@@ -4,15 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.{RatingBar, Toast}
 
 class Formular12Activity extends AppCompatActivity {
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_formular12)
+    ratingBar = findViewById(R.id.ratingBar).asInstanceOf[RatingBar]
   }
 
   def goBack(view: View): Unit = {
     val prevView = new Intent(this, classOf[SelectActivity])
     startActivity(prevView)
   }
+
+  // Initialize RatingBar
+  var ratingBar: RatingBar = null
+
+  /**
+    * Display rating by calling getRating() method.
+    * @param view
+    */
+  def rateMe(view:View){
+
+    Toast.makeText(getApplicationContext(), String.valueOf(ratingBar.getRating()), Toast.LENGTH_LONG).show();
+  }
+
+
+
+
+
 }
