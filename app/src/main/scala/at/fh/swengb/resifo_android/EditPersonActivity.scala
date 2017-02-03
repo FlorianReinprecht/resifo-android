85695888f03a1ea548b05aa07d2eb15eef594b1f
haxhaagn15 <agnesa.haxha@edu.fh-joanneum.at>
2017-02-03 10:13:47 +0100

    Improve the Edit-Person-Activity

--- app/src/main/scala/at/fh/swengb/resifo_android/EditPersonActivity.scala
+++ app/src/main/scala/at/fh/swengb/resifo_android/EditPersonActivity.scala
@@ -1,48 +1,47 @@
 package at.fh.swengb.resifo_android
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 
 
-
-
 class EditPersonActivity extends AppCompatActivity {
-
+/*
   var person:Person;
-
+*/
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_edit_person)
+/*
+    val intent: Intent = getIntent
 
-    val intent: Nothing = getIntent
+    person = intent.getExtras.get("person").asInstanceOf[String]
 
-    person = intent.getExtras.get("person").asInstanceOf[Nothing]
-
-    val viewAnrede: Nothing = findViewById(R.id.textView_anrede).asInstanceOf[Nothing]
-    val viewFamiliennameVor: Nothing = findViewById(R.id.textView_familiennameVor).asInstanceOf[Nothing]
-    val viewName: Nothing = findViewById(R.id.textView_name).asInstanceOf[Nothing]
-    val viewGeburt: Nothing = findViewById(R.id.textView_geburt).asInstanceOf[Nothing]
-    val viewReligion: Nothing = findViewById(R.id.textView_religion).asInstanceOf[Nothing]
-    val viewFamilienstand: Nothing = findViewById(R.id.textView_familienstand).asInstanceOf[Nothing]
-    val viewStaatsangehoerigkeit: Nothing = findViewById(R.id.textView_staatsangehoerigkeit).asInstanceOf[Nothing]
-    val viewZmrZahl: Nothing = findViewById(R.id.textView_zmrZahl).asInstanceOf[Nothing]
-    val viewReisepassnummer: Nothing = findViewById(R.id.textView_reisepassnummer).asInstanceOf[Nothing]
-    val viewAusstellungsdatum: Nothing = findViewById(R.id.textView_ausstellungsdatum).asInstanceOf[Nothing]
-    val viewAusstellendeBehoerde: Nothing = findViewById(R.id.textView_ausstellendeBehoerde).asInstanceOf[Nothing]
-    val viewAnmStrasse: Nothing = findViewById(R.id.textView_anmStrasse).asInstanceOf[Nothing]
-    val viewAnmOrt: Nothing = findViewById(R.id.textView_anmOrt).asInstanceOf[Nothing]
-    val viewHauptwohnsitz: Nothing = findViewById(R.id.textView_hauptwohnsitz).asInstanceOf[Nothing]
-    val viewHauptStrasse: Nothing = findViewById(R.id.textView_hauptStrasse).asInstanceOf[Nothing]
-    val viewHauptOrt: Nothing = findViewById(R.id.textView_hauptOrt).asInstanceOf[Nothing]
-    val viewZuzug: Nothing = findViewById(R.id.textView_zuzug).asInstanceOf[Nothing]
-    val viewAbmStrasse: Nothing = findViewById(R.id.textView_abmStrasse).asInstanceOf[Nothing]
-    val viewAbmOrt: Nothing = findViewById(R.id.textView_abmOrt).asInstanceOf[Nothing]
-    val viewVerzug: Nothing = findViewById(R.id.textView_verzug).asInstanceOf[Nothing]
-    val viewUnterkunftNachname: Nothing = findViewById(R.id.textView_unterkunftNachname).asInstanceOf[Nothing]
-    val viewUnterkunftVorname: Nothing = findViewById(R.id.textView_unterkunftVorname).asInstanceOf[Nothing]
-    val viewUnterkunftDatum: Nothing = findViewById(R.id.textView_unterkunftDatum).asInstanceOf[Nothing]
+    val viewPersonAnrede: String = findViewById(R.id.textView_personAnrede).asInstanceOf[String]
+    val viewFamiliennameVor: String = findViewById(R.id.textView_familiennameVor).asInstanceOf[String]
+    val viewName: String = findViewById(R.id.textView_name).asInstanceOf[String]
+    val viewGeburt: String = findViewById(R.id.textView_geburt).asInstanceOf[String]
+    val viewReligion: String = findViewById(R.id.textView_religion).asInstanceOf[String]
+    val viewFamilienstand: String = findViewById(R.id.textView_familienstand).asInstanceOf[String]
+    val viewStaatsangehoerigkeit: String = findViewById(R.id.textView_staatsangehoerigkeit).asInstanceOf[String]
+    val viewZmrZahl: String = findViewById(R.id.textView_zmrZahl).asInstanceOf[String]
+    val viewReisepassnummer: String = findViewById(R.id.textView_reisepassnummer).asInstanceOf[String]
+    val viewAusstellungsdatum: String = findViewById(R.id.textView_ausstellungsdatum).asInstanceOf[String]
+    val viewAusstellendeBehoerde: String = findViewById(R.id.textView_ausstellendeBehoerde).asInstanceOf[String]
+    val viewAnmStrasse: String = findViewById(R.id.textView_anmStrasse).asInstanceOf[String]
+    val viewAnmOrt: String = findViewById(R.id.textView_anmOrt).asInstanceOf[String]
+    val viewHauptwohnsitz: String = findViewById(R.id.textView_hauptwohnsitz).asInstanceOf[String]
+    val viewHauptStrasse: String = findViewById(R.id.textView_hauptStrasse).asInstanceOf[String]
+    val viewHauptOrt: String = findViewById(R.id.textView_hauptOrt).asInstanceOf[String]
+    val viewZuzug: String = findViewById(R.id.textView_zuzug).asInstanceOf[String]
+    val viewAbmStrasse: String = findViewById(R.id.textView_abmStrasse).asInstanceOf[String]
+    val viewAbmOrt: String = findViewById(R.id.textView_abmOrt).asInstanceOf[String]
+    val viewVerzug: String = findViewById(R.id.textView_verzug).asInstanceOf[String]
+    val viewUnterkunftNachname: String = findViewById(R.id.textView_unterkunftNachname).asInstanceOf[String]
+    val viewUnterkunftVorname: String = findViewById(R.id.textView_unterkunftVorname).asInstanceOf[String]
+    val viewUnterkunftDatum: String = findViewById(R.id.textView_unterkunftDatum).asInstanceOf[String]
 
 
     val viewAnrede.setText(person.getAnrede)
@@ -53,24 +52,18 @@ class EditPersonActivity extends AppCompatActivity {
     val viewFamilienstand.setText(person.getFamilienstand)
     val viewStaatsangehoerigkeit.setText(person.getStaatsangehoerigkeit)
     val viewZmrZahl.setText(person.getZmrZahl)
-
-
     val viewReisepassnummer.setText(person.getReisepassnummer)
     val viewAusstellungsdatum.setText(person.getAusstellungsdatum)
     val viewAusstellendeBehoerde.setText(person.getAusstellendeBehoerde)
-
     val viewAnmStrasse.setText(person.getAnmStrasse)
     val viewAnmOrt.setText(person.getAnmOrt)
     val viewHauptwohnsitz.setText(person.getHauptwohnsitz)
-
     val viewHauptStrasse.setText(person.getHauptStrasse)
     val viewHauptOrt.setText(person.getHauptOrt)
     val viewZuzug.setText(person.getZuzug)
-
     val viewAbmStrasse.setText(person.getAbmStrasse)
     val viewAbmOrt.setText(person.getAbmOrt)
     val viewVerzug.setText(person.getVerzug)
-
     val viewUnterkunftNachname.setText(person.getUnterkunftNachname)
     val viewUnterkunftVorname.setText(person.getUnterkunftVorname)
     val viewUnterkunftDatum.setText(person.getUnterkunftDatum)
@@ -81,7 +74,7 @@ class EditPersonActivity extends AppCompatActivity {
 
   def saveObject(view: View): Unit = {
     val zusammenfassung: String = findViewById(R.id.textView_zusammenfassung).asInstanceOf[EditText].getText.toString
-
+*/
   }
 
 
